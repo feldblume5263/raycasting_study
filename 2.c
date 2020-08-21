4e7ad5165b9ff585edb78d140a982e10c8ccf410
feldblume5263 <53016167+feldblume5263@users.noreply.github.com>
2020-08-21 16:55:12 +0900

    update ex2

--- 2.c
+++ 2.c
@@ -3,12 +3,38 @@
 
 #include "./mlx/mlx.h"
 
+#define X_EVENT_KEY_PRESS	2
+#define X_EVENT_KEY_RELEASE	3
+#define X_EVENT_KEY_EXIT	17
+
+// key num은 지정되어 있는 듯.
+#define KEY_W	13
+#define KEY_S	1
+#define KEY_ESC	53
+
 typedef struct	s_param
 {
 	int			x;
-	int			y;
-	char 		str[3];
 }				t_param;
+
+void			param_init(t_param *param)
+{
+	param->x = 3;
+}
+
+int				key_press(int keycode, t_param *param)
+{
+	static int	a = 0;
+
+	if (keycode == KEY_W)
+		param->x++;
+	else if (keycode == KEY_S)
+		param->x--;
+	else if (keycode == KEY_ESC)
+		exit(0);
+	printf("x: %d\n", param->x);
+	return (0);
+}
 
 int				main(void)
 {
@@ -21,11 +47,12 @@ int				main(void)
 	win = mlx_new_window(mlx, 720, 540, "mlx");
 	printf("-------------------------------\n");
 	printf("'W key': Add 1 to x.\n");
-	printf("'A key': Subtract 1 from x\n");
+	printf("'S key': Subtract 1 from x\n");
 	printf("'ESC key': Exit this program\n");
 	printf("'Other keys': print current x \n");
 	printf("-------------------------------\n");
 	printf("Current x = 3\n");
 
-	mlx_hook()
+	mlx_hook(win, X_EVENT_KEY_PRESS, 0, &key_press, &param);
+	mlx_loop(mlx);
 }
